afa86b6e4c2097c56995d365a8cc79809f1271b6
LLeo93 <liba.leoncini@gmail.com>
2026-02-09 19:41:07 +0100

    array completati, domani passo agli oggetti

---
+++ javascript/14-Lab-Boolean-Check-Function/script.js
@@ -0,0 +1,15 @@
+const booWho = (franco) => {
+  if (franco === true || franco === false) {
+    return true;
+  } else {
+    return false;
+  }
+};
+/*const booWho = (franco) => {
+  if (typeof franco === "boolean") {
+    return true;
+  } else {
+    return false;
+  }
+};
+*/
